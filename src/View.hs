01b75852e37abd198c98445e55ab735cbe8e02c6
mvolleyServer <mvolley@server>
2021-05-14 11:41:03 +0200

    refactor view by deleting unnecessary functions

--- src/View.hs
+++ src/View.hs
@@ -2,10 +2,12 @@ module View where
 
 import Grid
 import Control.Concurrent
+import Data.List
 
 sampleGrid :: [(Int,Int)]
 sampleGrid = [(2,2),(3,2),(4,2),(2,4),(3,4),(4,4),(4,3),(5,3)]
 
+-- drawPoints :: [(Int,Int)] -> [[Char]] -> [[Char]]
 
 {-- draws a point in a grid. Does not draw point if outside grid --}
 drawPoint :: (Int,Int) -> [[Char]] -> [[Char]] 
@@ -18,47 +20,9 @@ replace :: Int -> a -> [a] -> [a]
 replace pos newElem list 
   | pos >= length list || pos < 0 = list
   | otherwise = let (left, right) = splitAt pos list in 
-                left ++ [newElem] ++ tail right
-
-
-
-
-
-
-
-
-
-
-
-
-
+                left ++ [newElem] ++ (tail right)
 
 {-- old --}
-
-grid :: Int -> Int -> [[Char]]
-grid x y = let row = ['_' | _<-[0..x]] 
-           in [row | _<-[0..y]]
-
-ranges :: [(Int,Int)] -> (Int,Int)
-ranges grid = let firsts  = map (\(f,s) -> f) grid
-                  seconds = map (\(fs,sn) -> sn) grid
-              in 
-                  (maximum firsts,maximum seconds) 
-
-fillGrid :: [(Int,Int)] -> [[Char]]
-fillGrid grd = insertCells grd $ grid (fst $ ranges grd) (snd $ ranges grd)
-
-showGrid :: [[Char]] -> [IO ()]
-showGrid grd | null grd = []
-             | otherwise = (putStrLn $ show $ head grd):(showGrid $ tail grd)
-
-asdf :: [(Int,Int)] -> IO ()
-asdf cells = do  
-    result <- sequence $ showGrid (fillGrid cells) 
-    threadDelay (1000000)
-    asdf (updateGrid cells)
-    return ()
-              
 
 insertCells :: [(Int,Int)]->[[Char]]->[[Char]]
 insertCells cells grid | null $ tail cells = insertCell (head cells) grid
